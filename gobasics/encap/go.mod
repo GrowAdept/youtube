0f3d05f48d28614a11689d6b7601564e9600b825
Justin White <jwhite1337@protonmail.com>
2022-02-17 21:02:17 -0600

    changed visibility of Hello function

---
+++ gobasics/encap/go.mod
@@ -0,0 +1,5 @@
+module github.com/GrowAdept/youtube/gobasics/encap
+
+go 1.16
+
+require github.com/GrowAdept/youtube v0.0.0-20220218025656-9e945b32362b
---
+++ gobasics/encap/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+	calendar.hello()
+	/*
+		var d calendar.Date
+		d.SetYear(2022)
+		fmt.Println(d.Year)
+	*/
+}
