e92424506dfa4d8bf6e3079c704b021392498b2b
Justin White <jwhite1337@protonmail.com>
2022-02-21 17:46:49 -0600

    added package scope function for demo

--- gobasics/calendar/calendar.go
+++ gobasics/calendar/calendar.go
@@ -1,6 +1,9 @@
 package calendar
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // notice Date is capitalized and visible out side of package
 // notice fields are not capitalized and are not visible outside of package (package scope)
@@ -19,16 +22,28 @@ func Hello() {
 // Setter methods require * to change fields
 // Go convention for naming setter methods is SetFieldname
 // Setter methods can be used to force validation of data
-func (d *Date) SetYear(y int) {
+func (d *Date) SetYear(y int) error {
+	if y < 0 {
+		return errors.New("negative value for year")
+	}
 	d.year = y
+	return nil
 }
 
-func (d *Date) SetMonth(m int) {
+func (d *Date) SetMonth(m int) error {
+	if m < 1 || m > 12 {
+		return errors.New("value for month less than 1 or greater than 12")
+	}
 	d.month = m
+	return nil
 }
 
-func (d *Date) SetDay(day int) {
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("value for day less than 1 or greater than 31")
+	}
 	d.Day = day
+	return nil
 }
 
 // If any methods uses a * then it's idomatic in Go to use * in all that types methods
@@ -44,5 +59,11 @@ func (d *Date) GetDay() int {
 }
 
 func (d *Date) DisplayWorldDate() {
+	d.printMessage("DisplayWorldDate function running")
 	fmt.Printf("%v-%v-%v", d.year, d.month, d.Day)
+}
+
+// unexported method can be run inside of local package but not outside
+func (d *Date) printMessage(s string) {
+	fmt.Println(s)
 }
