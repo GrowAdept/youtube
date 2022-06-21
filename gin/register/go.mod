29d6bc2145e7293ac5a364b520fca6502834fcfc
Justin White <jwhite1337@protonmail.com>
2022-06-21 11:27:53 -0500

    add resources for register vid

--- gin/register/go.mod
+++ gin/register/go.mod
@@ -1,11 +1,13 @@
-module github.com/GrowAdept/youtube/gin/register
+module register
 
 go 1.18
 
 require (
+	github.com/AfterShip/email-verifier v1.3.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/sessions v1.2.1
+	github.com/wagslane/go-password-validator v0.3.0
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 )
 
@@ -16,6 +18,7 @@ require (
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/hbollon/go-edlib v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -29,4 +32,5 @@ require (
 	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	h12.io/socks v1.0.3 // indirect
 )
--- gin/register/main.go
+++ gin/register/main.go
@@ -1,58 +1,56 @@
 package main
 
 import (
-	"encoding/gob"
+	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/GrowAdept/youtube/gin/register/models"
-
-	"github.com/gorilla/sessions"
-	"golang.org/x/crypto/bcrypt"
-
+	emailverifier "github.com/AfterShip/email-verifier"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var store = sessions.NewCookieStore([]byte("super-secret"))
+const minEntropyBits = 60
+
+var (
+	verifier = emailverifier.NewVerifier()
+)
 
 func init() {
-	store.Options.HttpOnly = true // since we are not accessing any cookies w/ JavaScript, set to true
-	store.Options.Secure = true   // requires secuire HTTPS connection
-	gob.Register(&models.User{})
+	verifier = verifier.EnableDomainSuggest()
+	/*
+		port 25 is blocked for most at home ISP so EnableSMTPCheck() needs to be blocked
+		at home, hosting providers have port 25 open or allow it to be opened
+	*/
+	if servLoc == "remote" {
+		verifier = verifier.EnableSMTPCheck() // enable for remote production server
+	}
+	dispEmailsDomains := MustDispEmailDom()
+	verifier = verifier.AddDisposableDomains(dispEmailsDomains)
 }
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*/*.html")
-	var err error
 
 	router.GET("/", indexHandler)
 	router.GET("/login", loginGEThandler)
 	router.POST("/login", loginPOSThandler)
+	router.GET("/register", registerGEThandler)
+	router.POST("/register", registerPOSThandler)
+	router.GET("/emailver/:username/:verPass", emailverGEThandler)
 
 	authRouter := router.Group("/user", auth)
 	authRouter.GET("/profile", profileHandler)
+	authRouter.GET("/logout", logoutHandler)
 
-	err = router.Run("localhost:8080")
+	err := router.Run("localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-// auth middleware
-func auth(c *gin.Context) {
-	fmt.Println("auth middleware running")
-	session, _ := store.Get(c.Request, "session")
-	fmt.Println("session:", session)
-	_, ok := session.Values["user"]
-	if !ok {
-		c.HTML(http.StatusForbidden, "login.html", nil)
-		c.Abort()
-		return
-	}
-	fmt.Println("middleware done")
-	c.Next()
 }
 
 // index page
@@ -67,17 +65,16 @@ func loginGEThandler(c *gin.Context) {
 
 // loginPOSThandler verifies login credentials
 func loginPOSThandler(c *gin.Context) {
-	var user models.User
+	var user User
 	user.Username = c.PostForm("username")
 	password := c.PostForm("password")
-	err := user.GetUserByUsername()
+	err := user.getUserByUsername()
 	if err != nil {
 		fmt.Println("error selecting pswd_hash in db by Username, err:", err)
 		c.HTML(http.StatusUnauthorized, "login.html", gin.H{"message": "check username and password"})
 		return
 	}
 	err = bcrypt.CompareHashAndPassword([]byte(user.pswdHash), []byte(password))
-	fmt.Println("err from bycrypt:", err)
 	if err == nil {
 		session, _ := store.Get(c.Request, "session")
 		// session struct has field Values map[interface{}]interface{}
@@ -87,14 +84,22 @@ func loginPOSThandler(c *gin.Context) {
 		c.HTML(http.StatusOK, "loggedin.html", gin.H{"username": user.Username})
 		return
 	}
+	fmt.Println("err:", err)
 	c.HTML(http.StatusUnauthorized, "login.html", gin.H{"message": "check username and password"})
+}
+
+// logoutHandler logout user by deleting session data
+func logoutHandler(c *gin.Context) {
+	session, _ := store.Get(c.Request, "session")
+	delete(session.Values, "user")
+	session.Save(c.Request, c.Writer)
+	c.HTML(http.StatusOK, "login.html", gin.H{"message": "Logged out"})
 }
 
 // profileHandler displays profile information
 func profileHandler(c *gin.Context) {
-	fmt.Println("profile handler running")
 	session, _ := store.Get(c.Request, "session")
-	var user = &models.User{}
+	var user = &User{}
 	val := session.Values["user"]
 	var ok bool
 	if user, ok = val.(*User); !ok {
@@ -103,4 +108,105 @@ func profileHandler(c *gin.Context) {
 		return
 	}
 	c.HTML(http.StatusOK, "profile.html", gin.H{"user": user})
+}
+
+func registerGEThandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "register.html", nil)
+}
+
+func registerPOSThandler(c *gin.Context) {
+	var u User
+	u.Username = c.PostForm("username")
+	u.Email = c.PostForm("email")
+	u.password = c.PostForm("password")
+	// validate username
+	err := u.validateUsername()
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "register.html", gin.H{
+			"message": err,
+			"user":    u,
+		})
+		return
+	}
+	// validate password
+	err = u.validatePassword()
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "register.html", gin.H{
+			"message": err,
+			"user":    u,
+		})
+		return
+	}
+	// validate email
+	var statusCode int
+	statusCode, err = u.validateEmail()
+	if err != nil {
+		c.HTML(statusCode, "register.html", gin.H{
+			"message": err,
+			"user":    u,
+		})
+		return
+	}
+	// check if username already exists
+	exists := u.UsernameExists()
+	if exists {
+		c.HTML(http.StatusBadRequest, "register.html", gin.H{
+			"message": "Username already taken, please try another",
+			"user":    u,
+		})
+		return
+	}
+	// create user pswd hash and save user data
+	err = u.New()
+	if err != nil {
+		fmt.Println("create.New err:", err)
+		err = errors.New("there was an issue creating account, please try again")
+		c.HTML(http.StatusBadRequest, "register.html", gin.H{
+			"message": err,
+			"user":    u,
+		})
+		return
+	}
+	c.HTML(http.StatusOK, "register-succ.html", gin.H{})
+}
+
+func emailverGEThandler(c *gin.Context) {
+	var u User
+	u.Username = c.Param("username")
+	linkVerPass := c.Param("verPass")
+	err := u.getUserByUsername()
+	if err != nil {
+		fmt.Println("error selecting ver_hash in db by Username, err:", err)
+		c.HTML(http.StatusUnauthorized, "register-succ.html", gin.H{"message": "Please try link in verification email again"})
+		return
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(u.verHash), []byte(linkVerPass))
+	if err == nil {
+		// update user.Active to true
+		err = u.MakeActive()
+		if err != nil {
+			c.HTML(http.StatusBadRequest, "acc-activated.html", gin.H{
+				"message": "Please try email confirmation link again",
+			})
+			return
+		}
+		c.HTML(http.StatusOK, "acc-activated.html", nil)
+		return
+	}
+	c.HTML(http.StatusUnauthorized, "register-succ.html", gin.H{"message": "Please try link in verification email again"})
+}
+
+// disposable email list from
+// https://github.com/disposable-email-domains/disposable-email-domains/blob/master/disposable_email_blocklist.conf
+func MustDispEmailDom() (dispEmailDomains []string) {
+	file, err := os.Open("disposable_email_blocklist.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		dispEmailDomains = append(dispEmailDomains, scanner.Text())
+	}
+	return dispEmailDomains
 }
